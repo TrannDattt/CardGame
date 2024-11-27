79b40261ea7339af869adad533af01534d0f9ffc
TrannDattt <dt17949@gmail.com>
2024-11-27 23:03:32 +0700

    Push again 2

---
+++ Assets/Scripts/ServiceLocator/CardManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public void OnSelected()
+    {
+
+    }
+
+    private void Attack(Card card)
+    {
+
+    }
+
+    private void Defend(Card card)
+    {
+
+    }
+
+    private void FlipCard()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
